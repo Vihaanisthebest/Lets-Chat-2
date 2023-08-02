3cf213f745bbcecd3cbb0d772b986bfa7807c232
Vihaanisthebest <129866559+Vihaanisthebest@users.noreply.github.com>
2023-08-02 18:10:23 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDcI5TeK2iCwzX3BZrFgP_fRiB5vERbIAU",
+    authDomain: "project-94-da7f1.firebaseapp.com",
+    projectId: "project-94-da7f1",
+    storageBucket: "project-94-da7f1.appspot.com",
+    messagingSenderId: "97003834719",
+    appId: "1:97003834719:web:910c9482fff34405bcbaea"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
